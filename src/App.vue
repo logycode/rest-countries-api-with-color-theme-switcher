33ddf76fe90be2cc78e9ec9315a78fde17ac85b3
Tim Kranich <tim@logyco.de>
2022-04-23 19:32:34 +0200

    finished header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div id="app" :dark-mode="darkMode">
+    <header>
+      <h1>Where in the world</h1>
+      <button class="btn" @click="darkMode = !darkMode">
+        <div v-if="darkMode == false">
+          <font-awesome-icon icon="fa-regular fa-moon" />
+          <span>Dark Mode</span>
+        </div>
+        <div v-if="darkMode == true">
+          <font-awesome-icon v-if="darkMode == true" icon="fa-regular fa-sun" />
+          <span>Light Mode</span>
+        </div>
+      </button>
+    </header>
   </div>
 </template>
 
 <style lang="scss">
+$font: "Nunito Sans", sans-serif;
+$light-color: #fff;
+$dark-color: #2c3e50;
+
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: $font;
+  color: $dark-color;
+}
+#app[dark-mode="true"] {
+  color: $light-color;
+  background-color: $dark-color;
 }
 
-nav {
-  padding: 30px;
+header {
+  padding: 0 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* offset-x | offset-y | blur-radius | spread-radius | color */
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.056);
+}
+button {
+  height: 22px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+  font-weight: 600;
+  font-size: 16px;
+  color: inherit;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.svg-inline--fa {
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+  transform: rotate(-20deg);
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      darkMode: false,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faSun } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faMoon);
+library.add(faSun);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
